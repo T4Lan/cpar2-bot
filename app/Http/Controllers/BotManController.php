75473d532ea9f82588cd796065213c43ef1c267c
Matias Micheltorena <matias.micheltorena@gmail.com>
2018-04-24 15:22:25 -0300

    add map command and location message

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers;
 use BotMan\BotMan\BotMan;
 use Illuminate\Http\Request;
 use App\Conversations\ExampleConversation;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\BotMan\Messages\Attachments\Location;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
 class BotManController extends Controller
 {
+
+    public $earthEmoji = "\u{1F30F}";
+    private $grinningFace = "\u{1F601}";
+    
     /**
      * Place your BotMan logic here.
      */
@@ -33,5 +40,25 @@ class BotManController extends Controller
     public function startConversation(BotMan $bot)
     {
         $bot->startConversation(new ExampleConversation());
+    }
+
+    public function sendLocation(BotMan $bot)
+    {
+        // Make bot more human like
+        $bot->typesAndWaits(2);
+
+        $bot->reply('Esta es la dirección de Tecnópolis ' . $this->earthEmoji .$this->grinningFace);
+
+        // Create attachment
+        $attachment = new Location(-34.5614827695827, -58.50762742329734, [
+            'custom_payload' => true,
+        ]);
+        
+        // Build message object
+        $message = OutgoingMessage::create()
+                    ->withAttachment($attachment);
+        
+        // Reply message object
+        $bot->reply($message);
     }
 }
--- routes/botman.php
+++ routes/botman.php
@@ -3,12 +3,21 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
-});
+// $botman->hears('Hi', function ($bot) {
+//     $bot->reply('Hello!');
+// });
 
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+//$botman->hears('Start conversation', BotManController::class.'@startConversation');
 
+//test
 $botman->hears('/greet {text}', function($bot, $text){
     $bot->reply('Hola '.$text);
 });
+
+// fallback
+// $botman->fallback(function($bot) {
+//     $bot->reply('Perdon, no entiendo el comando que me mandaste..');
+// });
+
+// commands
+$botman->hears('/mapa',  BotManController::class.'@sendLocation');
